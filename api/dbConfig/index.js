9c821e447ea549510e8d4c46e47a11cb2ddab172
Ezell <lynellf@gmail.com>
2018-01-16 13:18:55 -0600

    added database routes

--- api/dbConfig/index.js
+++ api/dbConfig/index.js
@@ -1,13 +1,28 @@
-const api = require('express'),
-  router = api.Router(),
+const express = require('express'),
+  api = express(),
+  router = express.Router(),
+  session = require('express-session'),
+  mongoose = require('mongoose'),
+  MongoStore = require('connect-mongo')(session),
   User = require('./model/users'),
   database = require('mongoose'),
+  store = mongoose.connection,
   dbName = 'home',
   dbPort = 27017;
 
 database.connect(`mongodb://localhost:${dbPort}/${dbName}`, () =>
   console.log(`The database has initiated on port ${dbPort}`)
 );
+
+// Use sessions for tracking logins
+api.use(session({
+    secret: 'Ezell Frazier dot com',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: store,
+    }),
+}));
 
 // User registration
 router.post('/api/register', (req, res) => {
@@ -19,6 +34,22 @@ router.post('/api/register', (req, res) => {
             res.send(`${error}`);
         } else {
             return true
+        }
+    });
+});
+
+// User login
+router.post('/api/login', (req, res, next) => {
+    const userData = req.userData;
+
+    User.authenticate(userData, (error, user) => {
+        if (err || user) {
+            let err = new Error('Wrong email or password');
+            err.status = 401;
+            return res.send(err);
+        } else {
+            req.session.userId = user._id;
+            return res.send(true);
         }
     });
 });
